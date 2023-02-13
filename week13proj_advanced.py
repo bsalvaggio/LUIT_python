364baaf3912f5085d0326a6cdad4d5deaf898706
Bill Salvaggio <119374432+bsalvaggio@users.noreply.github.com>
2023-02-13 06:20:17 +0000

    commit

---
+++ week13proj_advanced.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# ADVANCED: Modify the script into a function such that any path can be passed as a parameter.
+#     This parameter should be optional and should default to working directory when the variable is not passed
+#     The function should be operational
